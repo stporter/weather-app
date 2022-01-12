f9269c7c0793d42b830c965e367797ee1a312e01
stporter <stporter2@gmail.com>
2022-01-12 12:57:19 -0500

    modal update

--- src/Modal/modal.js
+++ src/Modal/modal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Modal.css';
 
-const Modal = (props) => {
+function Modal(props) {
 	if (!props.show) {
 		return null;
 	}
@@ -26,6 +26,6 @@ const Modal = (props) => {
 			</div>
 		</div>
 	);
-};
+}
 
 export default Modal;
